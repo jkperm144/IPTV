d2152c63385020441243513cba8cca50d64d05a3
jkperm144 <jokopermana144@gmail.com>
2025-03-26 13:25:29 +0700

    Update Oppadrama.kt

--- cloudstream-extensions-hexated/DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
+++ cloudstream-extensions-hexated/DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
@@ -3,6 +3,6 @@ package com.hexated
 import com.lagradost.cloudstream3.extractors.Filesim
 
 class Oppadrama : DramaidProvider() {
-    override var mainUrl = "http://185.217.95.30"
+    override var mainUrl = "http://45.11.57.110"
     override var name = "Oppadrama"
 }
