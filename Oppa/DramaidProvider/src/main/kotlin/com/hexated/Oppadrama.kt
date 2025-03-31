93b36cb2a65bdf8e19e52e2b467399126155b61f
jkperm144 <jokopermana144@gmail.com>
2025-03-31 18:02:39 +0700

    add repo

---
+++ Oppa/DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
@@ -0,0 +1,8 @@
+package com.hexated
+
+import com.lagradost.cloudstream3.extractors.Filesim
+
+class Oppadrama : DramaidProvider() {
+    override var mainUrl = "http://176.97.113.111"
+    override var name = "Oppadrama"
+}
