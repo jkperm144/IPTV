03f82890bc4a148aa9abbf677397d96d4050df43
jkperm144 <jokopermana144@gmail.com>
2025-03-31 18:04:33 +0700

    Update Oppadrama.kt

--- Oppa/DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
+++ Oppa/DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
@@ -3,6 +3,6 @@ package com.hexated
 import com.lagradost.cloudstream3.extractors.Filesim
 
 class Oppadrama : DramaidProvider() {
-    override var mainUrl = "http://176.97.113.111"
+    override var mainUrl = "http://45.11.57.118"
     override var name = "Oppadrama"
 }
